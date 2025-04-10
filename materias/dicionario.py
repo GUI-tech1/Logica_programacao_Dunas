3a0760e982f2369bcacec753f615b1e2cb892adf
GUI-tech1 <guilhermeaquino897699@gmail.com>
2025-04-10 16:49:55 -0300

    estudo

--- materias/dicionario.py
+++ materias/dicionario.py
@@ -3,3 +3,34 @@ pessoas={"nome":"Clara","idade":18,"cidade":"Fortaleza"}
 print(pessoas["nome"])
 print(pessoas["idade"])
 print(pessoas["cidade"])
+
+#para adicionar um novo elemento no dicionario
+#pessoas["estado"]="Ceara"
+#print(pessoas["estado"])
+
+#para alterar um elemento do dicionario
+#pessoas["nome"]="Maria"
+#print(pessoas["nome"])
+
+#mostra as chaves do dicionario. EX: nome, idade e cidade
+#print(pessoas.keys())
+
+#mostra os valores do dicionario. EX: Clara, 18 e Fortaleza
+#print(pessoas.values())
+
+#retira uma Key do dicionario. EX: nome, idade e cidade
+#pessoas.pop("nome")
+#print(pessoas)
+
+#retira todos os elementos do dicionario
+#pessoas.clear()
+
+#verifica um elemento do dicionario. EX: nome, idade e cidade
+#if "nome" in pessoas:    #
+ #   print("Existe nome")
+#else:
+ #   print("Não existe nome")
+
+#verifica se existe um valor no dicionario. EX: Clara, 18 e Fortaleza
+#if "Clara" in pessoas.values():
+   # print("Existe Clara")
--- materias/loopwhile.py
+++ materias/loopwhile.py
@@ -1,4 +1,4 @@
 valores=0
-while valores<5:
-    print("contador é", valores)
+while valores<6:
+    print("valor é", valores)
     valores+=1 #tambem pode usar valores=+1
